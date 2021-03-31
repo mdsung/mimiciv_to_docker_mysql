9e426670f9df6754e53d462353694ac81dd628bb
mdsung <mdskylover@gmail.com>
2021-03-31 10:27:23 +0900

    make shell script to download mimic iv and load to docker mysql

---
+++ main.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+## Author: MinDong Sung
+## Objective: Download mimic-iv files and upload to mysql database
+
+## 0. CONST
+VER=1.0
+DOCKER_CONTAINER_NAME="mysql"
+## 1. read .env file
+export $(cat .env | xargs)
+
+## 2. download mimic file
+wget -r -N -c -np --user $PHYSIONET_USERNAME --password $PHYSIONET_KEY https://physionet.org/files/mimiciv/"$VER"/ 
+
+## 3. mv *.csv.gz file to parent folder
+mv ./physionet.org/files/mimiciv/"$VER"/core/*.csv.gz .
+mv ./physionet.org/files/mimiciv/"$VER"/hosp/*.csv.gz .
+mv ./physionet.org/files/mimiciv/"$VER"/icu/*.csv.gz .
+rm -r ./physionet.org
+
+## 4. unzip files
+gunzip -r .
+
+## 3. delete extra files
+rm *.html *.txt
+
+## 4. download sql files
+wget https://raw.githubusercontent.com/MIT-LCP/mimic-iv/master/buildmimic/mysql/index.sql 
+wget https://raw.githubusercontent.com/MIT-LCP/mimic-iv/master/buildmimic/mysql/load.sql 
+
+## 5. mkdir in docker root
+docker exec $DOCKER_CONTAINER_NAME mkdir mimic
+
+## 6. cp to docker mysql 
+FILES=./*
+
+for f in $FILES
+do
+  echo "Copying to mysql docker $f file..."
+  docker cp $f $DOCKER_CONTAINER_NAME:/mimic
+done
+
+## 7. run sql in the docker
+docker exec --workdir /mimic $DOCKER_CONTAINER_NAME mysql -u root --password=$MYSQL_PASSWORD --local-infile mimiciv < load.sql > load.log
+
+docker exec --workdir /mimic $DOCKER_CONTAINER_NAME mysql -u root --password=$MYSQL_PASSWORD --local-infile mimiciv < load.sql > load.log
